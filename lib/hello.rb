d6da57d46cf11b3b727094b1c82cd2bd8aeac640
Connor Cowan <ccowan003@comcast.net>
2019-06-06 23:53:49 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
+def hello_t(array)
+  if block_given?             #
+    i = 0                     #initialize counter
+    while i < array.length    #while condition
+      yield(array[i])         #
+      i += 1                  #increment
+    end
 
+    array                     #
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
-
+#call method here!
+  #ruby lib/hello.rb in terminal
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
