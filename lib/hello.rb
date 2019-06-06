a977f6d37eb336fcd19a3e28c53c8dd8c8085d1d
Connor Cowan <ccowan003@comcast.net>
2019-06-06 23:54:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -14,8 +14,8 @@ end
 
 #call method here!
   #ruby lib/hello.rb in terminal
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+#hello_t(["Tim", "Tom", "Jim"]) do |name|
+  #if name.start_with?("T")
+    #puts "Hi, #{name}"
+  #end
+#end
